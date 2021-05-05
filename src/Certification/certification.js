0c699a39770982f0a22edeb79249b719b3298c90
Bhavesh56789 <Bhavesh56789@users.noreply.github.com>
2021-05-05 14:30:42 +0530

    Java badge changed

--- src/Certification/certification.js
+++ src/Certification/certification.js
@@ -9,7 +9,7 @@ import Flash from 'react-reveal/Flash';
 class Certification extends Component {
   state = {
     certificate: [
-      { index: '1', title: 'ORACLE CORPORATION', year: '2021', course: 'Oracle Certified Java Professional', details: ["--Java SE 11", "--Examination 1Z0-819"], certificate: "https://drive.google.com/file/d/1HPhFOaXR4pmrOQ8jJ1s9WYwaf56jgkMj/view?usp=sharing", badge: "https://catalog-education.oracle.com/pls/certview/sharebadge?id=C828F6B0B5B04C6C61B1BDDA7D28F62A8246702C9F754CF0AB9BBC33F885AC11" },
+      { index: '1', title: 'ORACLE CORPORATION', year: '2021', course: 'Oracle Certified Java Professional', details: ["--Java SE 11", "--Examination 1Z0-819"], certificate: "https://drive.google.com/file/d/1HPhFOaXR4pmrOQ8jJ1s9WYwaf56jgkMj/view?usp=sharing", badge: "https://www.credly.com/badges/acbde724-3f12-4c74-9676-c403132390da/public_url" },
       { index: '2', title: 'FULL STACK WEB DEVELOPMENT', year: '2021', course: 'Hong Kong University of Science & Technology(Coursera)', details: '--HTML, CSS, Javascript, Bootstrap 4, Angular JS, NodeJS, Mongodb', certificate: "https://www.coursera.org/account/accomplishments/specialization/TSGYE475YGCA" },
       { index: '3', title: 'MICROSOFT TECHNOLOGY ASSOCIATE', year: '2020', course: ["Introduction to Programming Using JavaScript -Certified", "Examination 98382"], details: '--Javascript', certificate: "https://drive.google.com/file/d/14U_4ttNPNlvLVn35s1cI2Ifw7ih3JSUV/view?usp=sharing", badge: "https://www.credly.com/badges/1d90a468-aed3-49db-aaef-87b13a83dd0a/linked_in_profile" }
     ]
